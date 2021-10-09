3e35860cf04cc13d8117c3350cd25f97b7d8bc5a
jwtiyar <jwtiyar@gmail.com>
2021-10-09 14:05:26 +0300

    more do while

---
+++ dowhile.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  dowhile.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  09-Oct-21 13:34:27
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+int main(){
+int i=1;
+	do{
+		printf("%d",i);
+		i=i+1;
+
+	}while(i<1);
+}
